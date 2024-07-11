3c18d16417657ef1a898d7a0b30f74551ce79053
balamurugan634 <bala060304@gmail.com>
2024-07-12 02:49:10 +0530

    sigup page ui done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 
 import './App.css'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
 function App() {
 
   return (
-    <>frontend
-    </>
+    <div className='flex max-w-10xl mx-auto bg-black'>
+
+    <BrowserRouter>
+    <Routes>
+    <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
